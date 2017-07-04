89aa68f1f75b528415db98120edcedffb9d02a96
Eric Van Wyk <evw@umn.edu>
2017-07-03 22:49:14 -0500

    apt-get libgc-dev emacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ mv /home/ubuntu/motd /etc/motd
 mv /home/ubuntu/issue /etc/issue
 
 apt-get update
-apt-get install -y openjdk-8-jdk-headless ant git build-essential nailgun
+apt-get install -y openjdk-8-jdk-headless ant git build-essential nailgun 
+apt-get install -y libgc-dev emacs
 
 # People are going to be using this VM right? Should we consider vim/emacs?
 
