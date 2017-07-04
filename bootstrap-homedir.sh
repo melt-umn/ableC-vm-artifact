35e853e91bf558dfe1c65e92d520a74395173615
Eric Van Wyk <evw@cs.umn.edu>
2017-07-03 22:22:53 -0500

    adding more extensions

--- bootstrap-homedir.sh
+++ bootstrap-homedir.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if [ -d silver ] || [ -d ableC ]; then
-  rm -rf bin silver ableC extensions *.tar.gz || true
+  rm -rf bin silver ableC extensions ableC_sample_projects *.tar.gz || true
 fi
 
 # Download and set up Silver. (Presently using -latest.)
@@ -19,15 +19,29 @@ rm silver-latest.tar.gz
 
 git clone https://github.com/melt-umn/ableC.git
 
+
+# Get ableC sample projects repository
+
+git clone git@github.com:melt-umn/ableC_sample_projects.git
+rm -f ableC_sample_projects/Jenkinsfile
+
+
 # To help keep VM size down, I decided not to pre-build AbleC.
 
 # Download and set up the various extensions
 
 mkdir -p extensions
 cd extensions
+git clone https://github.com/melt-umn/ableC-algebraic-data-types.git
+git clone https://github.com/melt-umn/ableC-closure.git
 git clone https://github.com/melt-umn/ableC-condition-tables.git
 git clone https://github.com/melt-umn/ableC-cilk.git
-cd ~
+git clone https://github.com/melt-umn/ableC-halide.git
+git clone https://github.com/melt-umn/ableC-templating.git
+
+rm -f */Jenkinsfile
+
+cd ..
 
 # Anything else?
 
