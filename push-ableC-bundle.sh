174894816f89d91fbf7a30e6f26b329206217b3e
Lucas Kramer <krame505@umn.edu>
2019-03-16 15:37:37 -0500

    Add script to push everything

---
+++ push-ableC-bundle.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Name of directory where ableC is installed
+if [[ $# > 0 ]]; then
+    INSTALLDIR=$1
+elif [[ -d ableC && -d extensions ]]; then
+    INSTALLDIR=.
+else
+    INSTALLDIR="ableC-bundle"
+fi
+
+if [ ! -d ${INSTALLDIR} ]; then
+    echo "error: Bundle directory ${INSTALLDIR} does not exist"
+    exit 1
+fi
+
+cd ${INSTALLDIR}
+
+echo "Pushing Silver..."
+cd silver
+git push
+cd ..
+
+echo "Pushing ableC..."
+cd ableC
+git push
+cd ..
+
+echo "Pushing silver-ableC..."
+cd extensions/silver-ableC
+git push
+cd ../..
+
+echo "Pushing ableC projects..."
+for dir in ableC-*
+do
+    echo "Pushing $dir..."
+    (cd $dir && git push)
+done
+
+echo "Pushing extensions..."
+cd extensions
+for dir in ableC-*
+do
+    echo "Pushing $dir..."
+    (cd $dir && git push)
+done
